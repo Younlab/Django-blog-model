c475a5071aade36fdff7d0c54e93a39c1696edd4
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-20 14:27:49 +0900

    user models friends

--- app/blog/models.py
+++ app/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# 유저 모델
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    friends = models.ManyToManyField(
+        'self',
+    )
+
+    def __str__(self):
+        return self.name
+
+# 유저 정보
+class UserInfo(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=13)
